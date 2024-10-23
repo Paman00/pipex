b82ebc6d5006f66145fd793f2772f189c1958cb4
Miguel Angel Garcia Beltran <angelurano00@gmail.com>
2024-10-23 22:50:43 +0200

    Create basic struct example in pipex.h for command execution for execve

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: migugar2 <migugar2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 20:52:37 by migugar2          #+#    #+#             */
-/*   Updated: 2024/10/23 20:09:11 by migugar2         ###   ########.fr       */
+/*   Updated: 2024/10/23 22:49:56 by migugar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,12 @@
 # include <stdio.h> // perror
 # include <string.h> // strerror
 # include <sys/wait.h> // wait, waitpid
+
+typedef struct s_command
+{
+	char		**argv;
+	char		*cmd;
+	const char	*envp;
+}	t_command;
 
 #endif
